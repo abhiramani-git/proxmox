ff7e781d24943ad78981b8a67af723e94e2c1feb
abhiramani-git <91931006+abhiramani-git@users.noreply.github.com>
2025-11-11 20:13:26 +0000

    first

---
+++ renamehost.sh
@@ -0,0 +1,25 @@
+oldname=$1
+newname=$2
+
+
+files=(
+	/etc/hostname
+	/etc/hosts
+	/etc/postfix/main.cf
+)
+
+for file in "${files[@]}"; do
+	if [[ -f "$file" ]]; then
+		sed -i '' "s/${oldname}/${newname}/g" "$file"
+		echo "Updated: $file"
+	else
+		echo "Skipped (not found): $file" >&2
+	fi
+done
+
+
+mkdir /etc/pve/nodes/${newname}
+cp -r /etc/pve/nodes/${oldname}/* /etc/pve/nodes/${newname}/
+rm -rf /etc/pve/nodes/${oldname}
+
+reboot
