5ef4371b57272e28efdbddc9216ded9526d8d38c
abhiramani-git <91931006+abhiramani-git@users.noreply.github.com>
2025-11-11 20:31:31 +0000

    no remoot

--- renamehost.sh
+++ renamehost.sh
@@ -11,7 +11,9 @@ files=(
 for file in "${files[@]}"; do
 	if [[ -f "$file" ]]; then
 		sed -i '' "s/${oldname}/${newname}/g" "$file"
-		echo "Updated: $file"
+		echo "#####Printing: $file"
+        
+        cat $file
 	else
 		echo "Skipped (not found): $file" >&2
 	fi
@@ -22,4 +24,3 @@ mkdir /etc/pve/nodes/${newname}
 cp -r /etc/pve/nodes/${oldname}/* /etc/pve/nodes/${newname}/
 rm -rf /etc/pve/nodes/${oldname}
 
-reboot
