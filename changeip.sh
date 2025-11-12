f690e3fd6b6aebc4a12e9468ea2f6cc2ba0ab8d4
abhiramani-git <91931006+abhiramani-git@users.noreply.github.com>
2025-11-12 16:11:28 -0500

    Update changeip.sh

--- changeip.sh
+++ changeip.sh
@@ -1,3 +1,11 @@
+grep -ril "192.168.8" /etc/ 2>/dev/null
+# this command will list below files
 /etc/network/interfaces
-/etc/hosts
 /etc/resolv.conf
+/etc/hosts
+
+/etc/issue
+
+
+
+#/etc/issue gets created by system so not needed
